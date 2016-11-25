01ce3f1749312ebec2f96d4d3c91718435b9dfb3
SergeRegoor <serge@regoor.nl>
2016-11-25 20:43:27 +0100

    Added to Visual Studio

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/crypto-js/index.d.ts" />
+/// <reference path="globals/ip/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/strftime/index.d.ts" />
